2920d613b4005bd1cc8c81fa92381a83eb8d8c7d
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-02 10:40:50 +0300

    fix: complete tast1 module3

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,12 +2,12 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
+        int variable = 10;
         int variableOne = 1;
         int variableTwo = 2;
         int variableThree = 3;
-        final int variableFour = 4;
-        final int variableFive = 5;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
         int VariableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
